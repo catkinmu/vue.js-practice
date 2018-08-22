d39038ac67a638fa02ea8a12c2ebf4e21ca39d84
catkinmu <992746366@qq.com>
2018-08-22 21:54:21 +0800

    完善路由demo

--- VueRouterDemo/main.js
+++ VueRouterDemo/main.js
@@ -1,8 +1,56 @@
 import Vue from 'vue';
+import VueRouter from 'vue-router'
 import App from './app.vue';
 
+Vue.use(VueRouter)
+
+const Routers = [
+    {
+        path: '/index',
+        meta: {
+            title: '首页'
+        },
+        component: (resolve) => require(['./views/index.vue'], resolve)
+    },
+    {
+        path: '/about',
+        meta: {
+            title: '关于'
+        },
+        component: (resolve) => require(['./views/about.vue'], resolve)
+    },
+    {
+        path: '/user/:id',
+        meta: {
+            title: '个人主页'
+        },
+        component: (resolve) => require(['./views/user.vue'], resolve)
+    },
+    {
+        path: '*',
+        redirect: '/index'
+    }
+]
+
+const RouterConfig = {
+    //使用 HTML5 的 History 路由模式
+    mode: 'history',
+    routes: Routers
+}
+
+const router = new VueRouter(RouterConfig)
+
+router.beforeEach((to, form, next) => {
+    window.document.title = to.meta.title
+    next()
+})
+
+router.afterEach((to, from, next) => {
+    window.scrollTo(0, 0)
+})
 new Vue({
     el: '#app',
+    router,
     render: h => {
         return h(App)
     }
