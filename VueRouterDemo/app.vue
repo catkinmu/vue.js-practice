0920c948c1cca29ad285c42a8f51a4ca93811d19
catkinmu <992746366@qq.com>
2018-08-22 20:41:41 +0800

    新增vue路由demo

---
+++ VueRouterDemo/app.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <v-title title="Vue组件化"></v-title>
+        <v-button @click="handleClick">点击按钮</v-button>
+        <p>
+            <img src="./images/image.png" style="width: 200px;">
+        </p>
+    </div>
+</template>
+<script>
+    import vTitle from './title.vue';
+    import vButton from './button.vue';
+
+    export default {
+        components: {
+            vTitle,
+            vButton
+        },
+        methods: {
+            handleClick (e) {
+                console.log(e);
+            }
+        }
+    }
+</script>
