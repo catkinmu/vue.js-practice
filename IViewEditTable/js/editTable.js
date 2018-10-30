6725ac895f6d6aaee47c7cc2015970f8bf50ae7a
catkinmu <992746366@qq.com>
2018-10-30 15:06:39 +0800

    添加一些注释

--- IViewEditTable/js/editTable.js
+++ IViewEditTable/js/editTable.js
@@ -15,18 +15,22 @@ var editButton = function(vm, h, currentRow, index) {
       margin: '0 5px'
     },
     on: {
-      click: function() {
-        var tempData = vm.handleBackdata(currentRow)
-        if (!currentRow.editting) {
-          currentRow.editting = true;
-        } else {
-          if (JSON.stringify(tempData) == JSON.stringify(vm.dataList[index])) {
-            console.log('未更改');
-            return currentRow.editting = false;
+      on: {
+        click: function() {
+          // 点击按钮时改变当前行的编辑状态,当数据被更新时,render函数会再次执行,详情参考https://cn.vuejs.org/v2/api/#render
+          // handleBackdata是用来删除当前行的editting属性与saving属性
+          var tempData = vm.handleBackdata(currentRow)
+          if (!currentRow.editting) {
+            currentRow.editting = true;
+          } else {
+            // 这里也是简单的点击编辑后的数据与原始数据做对比,一致则不做操作,其实更好的应该遍历所有属性并判断
+            if (JSON.stringify(tempData) == JSON.stringify(vm.dataList[index])) {
+              console.log('未更改');
+              return currentRow.editting = false;
+            }
+            vm.saveData(currentRow, index)
+            currentRow.saving = true;
           }
-          vm.saveData(currentRow, index)
-          currentRow.saving = true;
-          currentRow.editting = false;
         }
       }
     }
@@ -150,6 +154,7 @@ var vm = new Vue({
         title: '开工时间',
         titleHtml: '开工时间 <i class="ivu-icon ivu-icon-edit"></i>',
         key: 'DATE_START',
+        //这里在后面处理的时候会分割成['month','yyyy-MM']的数组,分别代表iview的DatePicker组件选择日期的格式与数据库传过来时页面显示的格式
         date: 'month_yyyy-MM',
         editable: true
       }, {
@@ -196,11 +201,11 @@ var vm = new Vue({
           item.saving = false;
           return item;
         });
-        self.init();
         self.loading = false;
       });
     },
     //初始化数据
+    //methods中添加
     init: function() {
       console.log('init');
       var self = this;
@@ -231,35 +236,36 @@ var vm = new Vue({
             return h('div', children);
           };
         }
+        //如果含有editable属性并且为true
         if (item.editable) {
           item.render = function(h, params) {
             var currentRow = self.cloneDataList[params.index];
+            // 非编辑状态
             if (!currentRow.editting) {
-              // 日期类型单独 渲染
+              // 日期类型单独 渲染(利用工具暴力的formatDate格式化日期)
               if (item.date) {
                 return h('span', self.utils.formatDate(currentRow[item.key], item.date.split('_')[1]))
               }
-              // 下拉类型单独渲染
+              // 下拉类型中value与label不一致时单独渲染
               if (item.option && self.utils.isArray(item.option)) {
-                if (typeof item.option[0] == 'object') {
-                  // console.log(item.option.find(findObjectInOption(currentRow[item.key])).label);
+                // 我这里为了简单的判断了第一个元素为object的情况,其实最好用every来判断所有元素
+                if (typeof item.option[0] === 'object') {
                   return h('span', item.option.find(findObjectInOption(currentRow[item.key])).label);
                 }
               }
               return h('span', currentRow[item.key]);
             } else {
+              // 编辑状态
+              //如果含有option属性
               if (item.option && self.utils.isArray(item.option)) {
                 return h('Select', {
                   props: {
+                    // ***重点***:  这里要写currentRow[params.column.key],绑定的是cloneDataList里的数据
                     value: currentRow[params.column.key]
                   },
                   on: {
                     'on-change': function(value) {
                       self.$set(currentRow, params.column.key, value)
-                      // self.$set(self.dataList[params.index], params.column.key, value)
-                      // console.log(self.dataList[params.index][params.column.key]);
-                      // currentRow[params.column.key] = value
-                      // self.dataList[params.column.key] = value
                     }
                   }
                 }, item.option.map(function(item) {
@@ -271,6 +277,7 @@ var vm = new Vue({
                   }, item.label || item);
                 }));
               } else if (item.date) {
+                //如果含有date属性
                 return h('DatePicker', {
                   props: {
                     type: item.date.split('_')[0] || 'date',
@@ -280,23 +287,22 @@ var vm = new Vue({
                   on: {
                     'on-change': function(value) {
                       self.$set(currentRow, params.column.key, value)
-                      // self.$set(self.dataList[params.index], params.column.key, value)
                     }
                   }
                 });
               } else {
+                // 默认input
                 return h('Input', {
                   props: {
+                    // type类型也是自定的属性
                     type: item.input || 'text',
+                    // rows只有在input 为textarea时才会起作用
                     rows: 3,
                     value: currentRow[params.column.key]
                   },
                   on: {
                     'on-change'(event) {
                       self.$set(currentRow, params.column.key, event.target.value)
-                      // self.$set(self.dataList[params.index], params.column.key, event.target.value)
-                      // currentRow[params.column.key] = value
-                      // self.dataList[params.column.key] = value
                     }
                   }
                 });
@@ -306,14 +312,16 @@ var vm = new Vue({
         }
       });
     },
-    // 保存数据
     saveData: function(currentRow, index) {
       var self = this;
+      // 修改当前的原始数据, 就不需要再从服务端获取了
       this.$set(this.dataList, index, this.handleBackdata(currentRow))
       // 需要保存的数据
-      // ajax
+      // 模拟ajax
       setTimeout(function() {
+        充值编辑与保存状态
         currentRow.saving = false;
+        currentRow.editting = false;
         self.$Message.success('保存完成');
         console.log(self.dataList);
       }, 1000)
@@ -328,7 +336,7 @@ var vm = new Vue({
         vm.$Message.success('删除成功');
       }, 1000)
     },
-    //还原数据
+    // 还原数据,用来与原始数据作对比的
     handleBackdata: function(object) {
       var clonedData = JSON.parse(JSON.stringify(object));
       delete clonedData.editting;
@@ -338,18 +346,6 @@ var vm = new Vue({
   },
   created: function() {
     this.getData();
-  },
-  mounted: function() {}
-  // ,watch: {
-  //   // dataList: function() {
-  //   //   this.init();
-  //   // }
-  //   // 深度监听
-  //   // dataList: {
-  //   //   handler: function() {
-  //   //     this.init();
-  //   //   },
-  //   //   deep: true
-  //   // }
-  // }
+    this.init();
+  }
 });
